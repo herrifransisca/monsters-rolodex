f2d27fefbea1c38bdc78e934eb49ad8686c86f5b
Herri Fransisca <herrifransisca@gmail.com>
2019-07-09 16:14:16 +0700

    Updates

---
+++ precache-manifest.f2bd10df61933afce542cd83e0a2fdbd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51ea074a984057861e8ea1a7ead4e039",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "adcfe14b7fa27179ed60",
+    "url": "/monsters-rolodex/static/css/main.b886e616.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "adcfe14b7fa27179ed60",
+    "url": "/monsters-rolodex/static/js/main.81e9e50e.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
